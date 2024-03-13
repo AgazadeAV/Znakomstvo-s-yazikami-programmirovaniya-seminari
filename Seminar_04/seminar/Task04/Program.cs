b9eb168b2de273b79eb163b74bc06130b511aaf8
Azer Agazade <azer.agazade05@gmail.com>
2024-03-13 22:42:54 +0400

    did some fixes

---
+++ Seminar_04/seminar/Task04/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте массив из N случайных целых чисел (N вводится с клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и делятся нацело на 7.
+
+int[] CreateRandomArray(int min, int max, int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        Console.Write(item + " ");
+    }
+
+    Console.WriteLine();
+}
+
+int CountNeededNumber(int[] array)
+{
+    int count = 0;
+
+    foreach (int item in array)
+    {
+        if (item % 10 == 1 && item % 7 == 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+Console.WriteLine("Input min value:");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input max value:");
+int maximum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input size value:");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateRandomArray(minimum, maximum, length);
+ShowArray(array);
+int count = CountNeededNumber(array);
+Console.WriteLine("Kоличество чисел, которые оканчиваются на 1 и делятся нацело на 7:");
+Console.WriteLine(count);
---
+++ Seminar_04/seminar/Task05/Program.cs
@@ -0,0 +1,59 @@
+﻿// Заполните массив на N (вводится с консоли, не более 8) случайных целых чисел от 0 до 9.
+// Сформируйте целое число, которое будет состоять из цифр из массива. Старший разряд
+// числа находится на 0-м индексе, младший – на последнем. 
+
+int[] CreateRandomArray(int min, int max, int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        Console.Write(item + " ");
+    }
+
+    Console.WriteLine();
+}
+
+int NumberFromArray(int[] array)
+{
+    int number;
+    int tenPower = 1;
+    int result = 0;
+
+    foreach(int item in array)
+    {
+        tenPower *= 10;
+    }
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        number = array[i] * tenPower;
+        tenPower /= 10;
+        result += number;
+    }
+
+    result /= 10;
+    return result;
+}
+
+Console.WriteLine("Input min value:");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input max value:");
+int maximum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input size value:");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateRandomArray(minimum, maximum, length);
+ShowArray(array);
+int number = NumberFromArray(array);
+Console.WriteLine("Целое число, которое состоит из цифр массива:");
+Console.WriteLine(number);
